eb0fdc548154dd36b90371caa828f1fb17e1cb19
Jonathan Osmond <jonathancosmond@gmail.com>
2020-09-20 13:19:49 -0400

    include frontend (kanban-frontend) and backend (kanban-backend) projects

---
+++ kanban-frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <signup />
+  </div>
+</template>
+
+<script>
+import Signup from './components/Signup'
+
+export default {
+  name: 'App',
+  components: {
+    Signup
+  }
+}
+</script>
+
+<style>
+</style>
